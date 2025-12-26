854a3a0a3fbe03a2bcb56e170aea7886fec09859
Sujeet Kumar <sk0990670@gmail.com>
2025-12-26 16:14:42 +0530

    Reusable JS

--- force-app/main/default/lwc/ldsUtils/ldsUtils.js
+++ force-app/main/default/lwc/ldsUtils/ldsUtils.js
@@ -1,3 +1,119 @@
-import { LightningElement } from 'lwc';
+export function add(a, b){
+    /** Process your logic */
+    return Number(a) + Number(b);
+}
 
-export default class LdsUtils extends LightningElement {}
+export function sub(a, b){
+    /** Process your logic */
+    return Number(a) - Number(b)
+}
+
+/**
+ * Reduces one or more LDS errors into a string[] of error messages.
+ * @param {FetchResponse|FetchResponse[]} errors
+ * @return {String[]} Error messages
+ */
+export function reduceErrors(errors) {
+    if (!Array.isArray(errors)) {
+        errors = [errors];
+    }
+
+    return (
+        errors
+            // Remove null/undefined items
+            .filter((error) => !!error)
+            // Extract an error message
+            .map((error) => {
+                // UI API read errors
+                if (Array.isArray(error.body)) {
+                    return error.body.map((e) => e.message);
+                }
+                // Page level errors
+                else if (
+                    error?.body?.pageErrors &&
+                    error.body.pageErrors.length > 0
+                ) {
+                    return error.body.pageErrors.map((e) => e.message);
+                }
+                // Field level errors
+                else if (
+                    error?.body?.fieldErrors &&
+                    Object.keys(error.body.fieldErrors).length > 0
+                ) {
+                    const fieldErrors = [];
+                    Object.values(error.body.fieldErrors).forEach(
+                        (errorArray) => {
+                            fieldErrors.push(
+                                ...errorArray.map((e) => e.message)
+                            );
+                        }
+                    );
+                    return fieldErrors;
+                }
+                // UI API DML page level errors
+                else if (
+                    error?.body?.output?.errors &&
+                    error.body.output.errors.length > 0
+                ) {
+                    return error.body.output.errors.map((e) => e.message);
+                }
+                // UI API DML field level errors
+                else if (
+                    error?.body?.output?.fieldErrors &&
+                    Object.keys(error.body.output.fieldErrors).length > 0
+                ) {
+                    const fieldErrors = [];
+                    Object.values(error.body.output.fieldErrors).forEach(
+                        (errorArray) => {
+                            fieldErrors.push(
+                                ...errorArray.map((e) => e.message)
+                            );
+                        }
+                    );
+                    return fieldErrors;
+                }
+                // UI API DML, Apex and network errors
+                else if (error.body && typeof error.body.message === 'string') {
+                    return error.body.message;
+                }
+                // JS errors
+                else if (typeof error.message === 'string') {
+                    return error.message;
+                }
+                // Unknown error shape so try HTTP status text
+                return error.statusText;
+            })
+            // Flatten
+            .reduce((prev, curr) => prev.concat(curr), [])
+            // Remove empty strings
+            .filter((message) => !!message)
+    );
+}
+
+export function executeApexWithLoading(component, methodName, params = {}, options={}){
+    let response = {
+        data: null,
+        error: null,
+        loading: false,
+        hasError: false,
+    }
+    try{
+        methodName(params)
+        .then((result) => {
+            // Handle success
+            response.data = result;
+            response.loading = false;
+        })
+        .catch((error) => {
+            response.error = error;
+            response.hasError = true;
+        })
+        .finally(() => {
+            // Handle finally
+        });
+    }catch(error){
+
+    } finally {
+
+    }
+}
